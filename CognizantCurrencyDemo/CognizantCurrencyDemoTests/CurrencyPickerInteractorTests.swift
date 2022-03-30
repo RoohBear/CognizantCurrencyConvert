eaa343997ab17d5b1364c4aed52e20627ec1f7a2
Greg Wishart <greg.wishart@gmail.com>
2022-03-30 17:07:37 -0400

    Replaced IntegrationTest with UnitTest using MockedScoopService

--- CognizantCurrencyDemo/CognizantCurrencyDemoTests/CurrencyPickerInteractorTests.swift
+++ CognizantCurrencyDemo/CognizantCurrencyDemoTests/CurrencyPickerInteractorTests.swift
@@ -2,12 +2,29 @@
 //  CurrencyPickerInteractorTests.swift
 //  CognizantCurrencyDemoTests
 //
-//  Created by Shinoy on 2022-03-30.
+//  Created by Greg on 2022-03-30.
 //
 
 import XCTest
 import Combine
 @testable import CognizantCurrencyDemo
+
+class ScoopServiceMock: CurrencyScoopServiceProtocol
+{
+    func getCurrencies() -> AnyPublisher<[Currency]?, Never> {
+        var x = [Currency]()
+        x.append(Currency(currencyCode:"greg", currencyName:"greg's fancy currency"))
+        return Just(x).eraseToAnyPublisher()
+    }
+    
+    func convertCurrency(from: String, to: String, amount: String) -> AnyPublisher<ConvertData?, Never> {
+        return Just(ConvertData(to: "CAD", value: 123.45)).eraseToAnyPublisher()
+    }
+    
+    func getCurrencyRates(base: String, latest: [String]) -> AnyPublisher<CurrencyRates?, Never> {
+        return Just(CurrencyRates(base: "USD", rates: ["USD":1.23])).eraseToAnyPublisher()
+    }
+}
 
 class CurrencyPickerInteractorTests: XCTestCase
 {
@@ -19,14 +36,13 @@ class CurrencyPickerInteractorTests: XCTestCase
     override func tearDownWithError() throws {
     }
 
-    // Downloads currencies from CurrencyScoopService.
-    // Asserts that the # of currencies downloaded is > 0
-    func testCurrencyPickerInteractor()
+    // same as above, but uses Mocked Scoop Service
+    func testCurrencyPickerInteractorMocked()
     {
-        let cpi = CurrencyPickerInteractor(currencyScoopService: CurrencyScoopService())
+        let cpi = CurrencyPickerInteractor(currencyScoopService: ScoopServiceMock())
         
         let expectation = expectation(description: "waiting for currency picker interactor")
-        let _ = cpi.getAllCurrencies().sink { [weak self] in
+        let _ = cpi.getAllCurrencies().sink {
             print("got something from scoop service")
             if let x = $0 {
                 XCTAssert(x.count > 0)
