f9872cfbf4b8ac8a8478327e5b9cef9f3a3e604d
Curtis-Stilwell <stilcurt@yahoo.com>
2022-04-01 14:25:35 -0500

    Created a basic UI Test for CurrencyPicker to navigate and check the navigation bar title.

---
+++ CognizantCurrencyDemo/CognizantCurrencyDemoUITests/CurrencyPickerUITests.swift
@@ -0,0 +1,42 @@
+//
+//  CurrencyPickerTests.swift
+//  CognizantCurrencyDemoUITests
+//
+//  Created by Curtis Stilwell on 4/1/22.
+//
+
+import XCTest
+
+class CurrencyPickerTests: XCTestCase {
+    
+    private var app: XCUIApplication!
+    private var apiKey: String!
+    private let enviornmentKey = "CURRENCY_SCOOP_API_KEY"
+    
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        
+        app = XCUIApplication()
+        apiKey = ProcessInfo.processInfo.environment[enviornmentKey]
+        app.launchEnvironment = [enviornmentKey: apiKey]
+
+        app.launch()
+        app.tabBars.buttons.element(boundBy: 1).tap()
+        app.navigationBars.buttons["Compose"].tap()
+    }
+    
+    override func tearDownWithError() throws {
+        app = nil
+        apiKey = nil
+    }
+    
+    func testCurrencyPickerExists_WhenBaseCurrencyCellTapped_ShouldShowCurrencyPickerController() {
+        let baseCell = app.tables.element(boundBy: 0).cells.element(boundBy: 0)
+        XCTAssertTrue(baseCell.waitForExistence(timeout: 5))
+       
+        baseCell.tap()
+        XCTAssert(app.navigationBars["Base Currency"].exists)
+    }
+    
+}
